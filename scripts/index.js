3f9399b8ec68953a743f7b6e75c343898a7b5965
Andrey Ovchinnikov <andpey.ovchinnikov@yandex.ru>
2024-06-18 11:48:06 +0300

    ГОТОВО

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,27 @@
 // @todo: Темплейт карточки
 
 const cardsTemplate = document.querySelector('#card-template').content
-const placeItem = document.querySelector('.places__item');
-
 
 // @todo: DOM узлы
 
 const placesList = document.querySelector('.places__list');
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// document.querySelector('.page').onclick = function(e) {
-//   if(e.target.className != 'card__delete-button') return
-//   let card = e.target.closest('.card')
-//   card.remove()
-// }
+const placeItem = document.querySelector('.places__item');
 
 // @todo: Вывести карточки на страницу
 
-// initialCards.forEach(function(element) {
-//   const cardElement = cardsTemplate.cloneNode(true);
-//   const cardDelete = cardsTemplate.querySelector('.card__delete-button');
+initialCards.forEach(function(element) {
+  const cardElement = createCard(element, deleteCard) 
+  placesList.append(cardElement)
+})
 
-//   cardElement.querySelector('.card__image').src = element.link;
-//   cardElement.querySelector('.card__title').textContent = element.name;
+// @todo: Функция удаления карточки
 
-//   placesList.append(cardElement)
+function deleteCard(event) {
+  let card = event.target.closest('.card')
+  card.remove()
+}
 
-//   return cardElement;
-
-// })
-
-
-// cardDelete.addEventListener('click', function()  {
-//   const handDeleteCard = cardDelete.closest('.card');
-//   card.remove()
-// });
-
+// @todo: Функция создания карточки
 
 function createCard(element, deleteCard) {
   const cardElement = cardsTemplate.cloneNode(true);
@@ -55,17 +38,3 @@ function createCard(element, deleteCard) {
   return cardElement;
 
 }
-
-
-
-
-initialCards.forEach(function(element) {
-  const cardElement = createCard(element, deleteCard) 
-  placesList.append(cardElement)
-})
-
-function deleteCard(event) {
-  let card = event.target.closest('.card')
-  card.remove()
-}
-
